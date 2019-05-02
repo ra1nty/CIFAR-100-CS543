34c27e63e01c2905eba5b738513b52ee7b0bb64b
Ra1n <ytang.me@gmail.com>
2019-05-02 13:04:01 -0500

    Update cutout.py

--- cutout.py
+++ cutout.py
@@ -1,18 +1,25 @@
 import numpy as np
 
-def CutOut(n, size):
-    def _cutout(img):
-        h, w = img.size[:2]
-        mask = np.ones((h, w, 3), np.float32)
-        for n in range(n):
-            offh = np.random.randint(h)
-            offw = np.random.randint(w)
-            x1 = np.clip(offw - size//2, 0, w)
-            x2 = np.clip(offw + size//2, 0, w)
-            y1 = np.clip(offh - size//2, 0, h)
-            y2 = np.clip(offh + size//2, 0, h)
-            mask[y1:y2, x1:x2] = [0., 0., 0.]
-        img = img * mask
-        return img
+class CutOut(object):
+    """Cutout an image tensor image with n holes with size length.
+    """
+    def __init__(self, n, length):
+        self.n = n
+        self.length = length
     
-    return _cutout
+    def __call__(self, tensor):
+        _, h, w = tensor.shape
+        mask = np.ones((3, h, w), dtype=np.float32)
+        for i in range(self.n):
+            y = np.random.randint(h)
+            x = np.random.randint(w)
+            y1 = np.clip(y - self.length // 2, 0, h)
+            y2 = np.clip(y + self.length // 2, 0, h)
+            x1 = np.clip(x - self.length // 2, 0, w)
+            x2 = np.clip(x + self.length // 2, 0, w)
+            mask[:, y1: y2, x1: x2] = 0.
+        tensor = tensor * torch.from_numpy(mask)
+        return tensor
+    
+    def __repr__(self):
+        return self.__class__.__name__ + '(n={0}, length={1})'.format(self.n, self.length)
